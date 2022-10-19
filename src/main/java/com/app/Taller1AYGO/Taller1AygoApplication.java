e5f041269a808ff244b78a2badbc33b787f58044
nontoa <nicolasnc010@gmail.com>
2022-10-19 18:37:57 -0500

    Initial Commit

---
+++ src/main/java/com/app/Taller1AYGO/Taller1AygoApplication.java
@@ -0,0 +1,13 @@
+package com.app.Taller1AYGO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller1AygoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taller1AygoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/Taller1AYGO/Taller1AygoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.Taller1AYGO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller1AygoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
