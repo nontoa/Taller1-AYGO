bd73ab627d39ab5a7bdf798a43c1dd4c5efb1646
nontoa <nicolasnc010@gmail.com>
2022-10-20 01:25:05 -0500

    Adding load balancer service

---
+++ App-LB-RoundRobin/src/main/java/com/app/AppLBRoundRobin/AppLbRoundRobinApplication.java
@@ -0,0 +1,13 @@
+package com.app.AppLBRoundRobin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLbRoundRobinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppLbRoundRobinApplication.class, args);
+	}
+
+}
---
+++ App-LB-RoundRobin/src/test/java/com/app/AppLBRoundRobin/AppLbRoundRobinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.AppLBRoundRobin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppLbRoundRobinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
